fe5bf3aa95f255a60c1ef604c6de4c16ab1771df
Fazhrul Sadip <fazhrul.sadip10@gmail.com>
2023-09-29 20:54:14 -0400

    updated

---
+++ Assignment1/Assignment1/Controllers/NumberMachineController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Assignment1.Controllers
+{
+    public class NumberMachineController : ApiController
+    {
+        public String[] Get(int id)
+        {
+            var add = id + 5;
+            var sub = id - 5;
+            var multiply = id * 10;
+            var div = id / 2;
+
+            return new String[] { add.ToString() , sub.ToString() , multiply.ToString(), div.ToString() };
+        }
+    }
+}
